6afb9ffcba2058d83c5dcd67387f3ef2a8d891c7
elliott dombrowski <elliottroyal@me.com>
2023-06-19 13:02:57 -0500

    proxying

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+import Books from './components/books';
+
 import './App.css';
 
+const API_URL = "http://localhost:3000/api/v1/books";
+
+const getAPIData = () => {
+  return axios.get(API_URL).then((response) => response.data);
+};
+
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then(items => {
+      if (mounted) {
+        setBooks(items);
+      };
+    });
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div className="App">
       <h1>testing</h1>
+      <Books books={books} />
     </div>
   );
 }
